96e4b642d7725f10fdf20d5f8c97880f3872d3fa
amsalimsema <salimsema@yahoo.com>
2022-03-18 12:56:02 +0300

    reactHooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { useState } from "react";
+import "./App.css";
+import Home from "./Home";
 
 function App() {
+  const [color, setColor] = useState("red");
+  console.log(color);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h2>Hooks</h2>
+        <h1>My favorite color is {color}!</h1>
+        <button type="button" onClick={() => setColor("blue")}>
+          Blue
+        </button>
+        <button type="button" onClick={() => setColor("red")}>
+          Red
+        </button>
+        <button type="button" onClick={() => setColor("pink")}>
+          Pink
+        </button>
+        <h2>Using custom hook</h2>
+        <Home />
+      </div>
+    </>
   );
 }
 
